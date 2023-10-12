724f253c8766853f6f01c36f3319e99bc54f17cd
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-12 23:43:19 +0000

    defining_func_parity

--- Conditionals_py/Parity.py
+++ Conditionals_py/Parity.py
@@ -4,3 +4,19 @@ if x % 2 == 0:
     print("x is an even number")
 else:
     print("x is an odd number")
+
+#Defining a function: is_even
+def main():
+    z = int(input("Enter the value of z:"))
+    if is_even(z):
+        print("z is an even number")
+    else:
+        print("z is an odd number")
+
+def is_even(y):
+    if y % 2 == 0:
+        return True
+    else:
+        return False
+    
+main()
