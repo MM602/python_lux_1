2fdcac8baa0f76bbb2605e16c8f3f8d25192e28b
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-14 20:57:41 +0000

    or_001

--- .devcontainer/house.py
+++ .devcontainer/house.py
@@ -10,3 +10,15 @@ if name == "Kelvin":
     print("Etihad")
 else:
     print("Name not identified")
+
+#Using the or function
+name = input("Enter your name")
+
+if name == "Haaland" or name == "Jack":
+    print("Etihad")
+if name == "Anthony":
+    print("Trafford")
+if name == "Kelvin":
+    print("Etihad")
+else:
+    print("Name not identified")
