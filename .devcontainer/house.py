cbdaf47d63e05ff1307b1fc12c1b248310fe89bc
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-14 20:55:25 +0000

    house_py_001

---
+++ .devcontainer/house.py
@@ -0,0 +1,12 @@
+name = input("Enter your name")
+
+if name == "Haaland":
+    print("Etihad")
+if name == "Jack":
+    print("Etihad")
+if name == "Anthony":
+    print("Trafford")
+if name == "Kelvin":
+    print("Etihad")
+else:
+    print("Name not identified")
