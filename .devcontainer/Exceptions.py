f0619293d15db13eb6cb57b7e8a36e8406803dce
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-20 23:48:10 +0000

    def_main_excp

--- .devcontainer/Exceptions.py
+++ .devcontainer/Exceptions.py
@@ -14,3 +14,19 @@ while True:
         break
 
 print(f"x is {x}")
+
+#Writing the code while defining a function
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("Enter the value of x:"))
+        except ValueError:
+            print("x is not a number")
+        else:
+            return x
+
+main()
