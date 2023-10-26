81e0e3ed66368c6728683c9c871f26a80da6046c
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-26 18:42:15 +0000

    update_choice_libraries

--- .devcontainer/libraries.py
+++ .devcontainer/libraries.py
@@ -2,3 +2,9 @@ import random
 
 coin = random.choice(["heads", "tails"])
 print(coin)
+
+#Using the specific library rather than a random one
+from random import choice
+
+coin = choice(["heads", "tails"])
+print(coin)
