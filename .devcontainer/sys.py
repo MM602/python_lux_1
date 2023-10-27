81633a617cbb3ee8c036083a734d7efa0027306e
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-27 22:14:50 +0000

    update_sys_py

--- .devcontainer/sys.py
+++ .devcontainer/sys.py
@@ -9,3 +9,13 @@ try:
     print("My name is", sys.argv[1])
 except IndexError:
     print("Invalid Argument")
+
+#More Updates
+import sys
+
+if len(sys.argv) < 2:
+    print("Too few characters")
+elif len(sys.argv) > 2:
+    print("Too many characters")
+else:
+    print("My name is", sys.argv[1])
