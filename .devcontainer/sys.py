2401299baabef39b0e61451472504e551a4e77ae
MM602 <140158418+MM602@users.noreply.github.com>
2023-10-27 22:11:36 +0000

    update

--- .devcontainer/sys.py
+++ .devcontainer/sys.py
@@ -1,3 +1,11 @@
 import sys
 
 print("My name is", sys.argv[1])
+
+#Updating the program to eliminate the error
+import sys
+
+try:
+    print("My name is", sys.argv[1])
+except IndexError:
+    print("Invalid Argument")
