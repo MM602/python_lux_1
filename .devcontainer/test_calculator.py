c7f00f25016f0fd708e27c437baef138639efa6b
MM602 <140158418+MM602@users.noreply.github.com>
2023-11-03 22:09:03 +0000

    unit tests_ py

---
+++ .devcontainer/test_calculator.py
@@ -0,0 +1,8 @@
+from calculator import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(11) == 121
+    assert square(-2) == 4
+    assert square(-4) == 16
